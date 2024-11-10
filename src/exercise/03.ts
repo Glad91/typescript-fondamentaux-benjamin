0369116bfe126921942bc5ca81e386b846024216
Gladys TRUILLET <gladys.truillet@gmail.com>
2024-11-10 22:15:11 +0100

    Trosiéme exercice

--- src/exercise/03.ts
+++ src/exercise/03.ts
@@ -9,25 +9,52 @@ init()
 // ✔️ Début de l'exercice
 
 // 🐶 Déclare une variable `person` (sans initialiser de valeur)
-// definie les propriétés / types suivants
+// définie les propriétés / types suivants
 //
 // - name de type string
 // - age de type number
 // - isActive de type boolean
 // - roles un tableau de string
-// - params un object non defini
+// - params un object non défini
 // - payload peut avoir n'importe quel type
 
-let person
+let person: {
+  name: string
+  age: number
+  isActive: boolean
+  roles: string[]
+  params: {}
+  payload: any
+  friend: {
+    name: string
+    age: number
+    isActive: boolean
+    roles: string[]
+    params: {}
+    payload: any
+  }
+}
 
 // 🐶 affecte des valeurs à person
 // laisse payload undefined
 person = {
-  //
+  payload: undefined,
+  params: {},
+  roles: ['ADMIN'],
+  name:'John',
+  age:30,
+  isActive: true,
+  friend: { isActive: false, name: 'Mike', params: {}, payload: undefined, roles: ['USER'],
+    age:45}
+
 }
 
 // 🤖 Affiche dans le navigateur grace à `displayText` le libellé suivant
 // 'John à 30 ans et a le role ADMIN'
+
+displayText(`${person.name} à ${person.age} ans et à le rôle ${person.roles.at(0)}`)
+
+displayText(`${person.name} est l'ami de ${person.friend.name}`)
 
 /*
 eslint
