fc7d1a8c5e44ab0303d794df1c06e50f9b5f157a
Gladys TRUILLET <gladys.truillet@gmail.com>
2024-11-10 21:48:03 +0100

    Deuxiéme exercice

--- src/exercise/02.ts
+++ src/exercise/02.ts
@@ -8,21 +8,29 @@ init()
 
 // ✔️ Début de l'exercice
 
-// 🐶 Créer une variable `names` mais sans initiliser de valeurs
-//  ajoute ensuite 'John' et 'doe' à `name` sans utiliser la méthode `push()`
+// 🐶 Créer une variable `names` mais sans initialiser de valeurs
+//  ajoute ensuite 'John' et 'doe' à `names` sans utiliser la méthode `push()`
 let names = []
+names = ['John', 'doe']
 
 // 🤖 affiche 'Il y a 2 noms' dans le navigateur grace à `displayText` et `.length`
+displayText(`Il y a ${names.length} noms`)
 
 // 🐶 Créer une constante `games` et initialise avec un tableau vide
 // avec la méthode `push()` ajoute 3 jeux
 // ⛏️ const games
 // 🤖 affiche le nombre d'élément à l'ecran
+const games = []
+games.push('Mario Kart','Call Of Duty','Zelda');
+
+displayText(`Il y a ${games.length} jeux dans le tableau`)
 
 // 🐶 Créer une constante `things` : un tableau qui peut prendre n'importe quel type de donnée
 // ⛏️ const things
-
+const things : any[] = [35, true, '21:44 coucou','hello world', false, {nom:'Ben', prenom:'Jamin'}]
 // 🤖 affiche le nombre d'élément à l'ecran
+
+displayText(`Il y a ${things.length} éléments dans le tableau`)
 
 /*
 eslint
